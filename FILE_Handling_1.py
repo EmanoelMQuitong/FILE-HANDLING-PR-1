047a18787a1d9aba5c38d5ce15e9d009072ec3ed
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-04-27 15:22:19 +0800

    #Determine if the number is even or odd

--- FILE_Handling_1.py
+++ FILE_Handling_1.py
@@ -3,5 +3,6 @@
 #List the contents of numbers.txt
 with open('numbers.txt', 'r') as f:
     for line in f:          #List all the contents of numbers.txt
- 
+        mod_num = int(line)%2   #Determine if the number is even or odd
+        
 
