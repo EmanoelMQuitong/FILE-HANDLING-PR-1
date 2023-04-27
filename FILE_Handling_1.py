03316391a32db058d2de2826a934a9d49367456f
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-04-27 12:45:21 +0800

    #List the contents of numbers.txt

--- FILE_Handling_1.py
+++ FILE_Handling_1.py
@@ -1,1 +1,7 @@
 #P-1( 25 points). Write a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+#List the contents of numbers.txt
+with open('numbers.txt', 'r') as f:
+    for line in f:
+        print(line)       
+
